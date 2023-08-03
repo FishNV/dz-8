ea1a3d4ee35db9e2525c7c457ee149c63abf39e2
Andrii Rybak <adnrii.rybak@amomedia.com>
2023-08-03 15:11:42 +0300

    dz-8 fixes

--- src/main/java/cc/robotdreams/Main.java
+++ src/main/java/cc/robotdreams/Main.java
@@ -1,9 +1,5 @@
 package cc.robotdreams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
         Student student1 = new Student("Андрій", "Рибак");
